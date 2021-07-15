38ede00fa77eaf4e37c90e0a7315b409b1a50641
chin <a5566qq2581@gmail.com>
2021-07-15 13:54:40 +0800

    fix: 機構未預設的問題
    機構雖然頁面顯示已選第一個(selectpicker)
    實際ngmodel的值是空值
    selectpicker跟model一起預設
    
    feat: blockui 2秒等待初始化完成
    
    feat: qrcode modal只能透過按ok關閉
    
    feat: qrcode modal新增顯示已建立的資料欄位
    
    feat: 更改qrcode modal大小
    
    refactor: 重構初始化邏輯

--- src/app/create-page/create-page.component.ts
+++ src/app/create-page/create-page.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, ChangeDetectionStrategy, KeyValueChanges, KeyValueDiffer, KeyValueDiffers, ChangeDetectorRef } from '@angular/core';
-import { joiTypeMessage, setInputFilter, verifyTWIdentifier } from '../shared/common';
-import { vaccineMap, agencyMap, ICDCData, dataTranslation , IVaccine } from './create-page-models';
+import { Component, OnInit, ChangeDetectionStrategy, KeyValueDiffers, ChangeDetectorRef, AfterViewInit, DoCheck } from '@angular/core';
+import { joiTypeMessage, verifyTWIdentifier } from '../shared/common';
+import { vaccineMap, agencyMap, ICDCData, dataTranslation, IVaccine } from './create-page-models';
 import * as _ from 'lodash';
 import * as joi from '@hapi/joi';
 import Swal from 'sweetalert2';
-import { FormBuilder, Validator, Validators } from '@angular/forms';
 import { CreatePageService } from './create-page.service';
 import { EUDCC1, VaccinationEntry } from '../shared/dgc-combined-schema.d';
 import genEDGCQR from '../shared/misc/edgcQRGenerator';
 import { CertResult } from '../shared/misc/edgcQRGenerator';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { qrcodeDataDecoder } from '../shared/misc/decoder/decoder';
 
 
 //import * as $ from 'jquery';
@@ -21,132 +19,46 @@ declare var $: any;
     styleUrls: ['./create-page.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CreatePageComponent implements OnInit {
+export class CreatePageComponent implements OnInit, AfterViewInit, DoCheck {
     @BlockUI() blockUI: NgBlockUI;
-    constructor(private differs: KeyValueDiffers,
-                private cdr: ChangeDetectorRef,
-                private createPageService : CreatePageService) { }
-
+    public isInit: boolean;
+    public isShowQrcode: boolean;
     public vaccineItem = vaccineMap;
-    public cdcData: ICDCData = {
-        AgencyCode: undefined,
-        Data: undefined
-    };
+    public cdcData: ICDCData;
     public agencyItem = agencyMap;
-    private dataSchema = {
-        AgencyCode: joi.required(),
-        IdNo: joi.string().required(),
-        Name: joi.string().required(),
-        Birthday: joi.string().required(),
-        InocuDate: joi.string().required(),
-        VaccID: joi.string().required(),
-        VaccDoses: joi.number().min(1).required()
-    };
-    public data = {
-        AgencyCode: undefined,
-        IdNo: undefined,
-        Name: undefined,
-        Birthday: undefined,
-        InocuDate: undefined,
-        VaccID: undefined,
-        VaccDoses: 1
-    }
-    private dataJoi = joi.object(this.dataSchema);
+    private dataSchema;
+    public data;
+    private dataJoi;
     private birthDateicker;
     private dateOfVaccinationDatePicker;
-    public selectedVaccItem : IVaccine = {
-        "name": "",
-        "id": "",
-        "type" : {
-            "id" : "" ,
-            "name" : ""
-        },
-        "org": {
-            "id": "",
-            "name": ""
-        }
-    };
+    public selectedVaccItem: IVaccine;
     public eudgc;
-    public qrCode = "123";
-    public TAN;
+    public qrCode: string;
+    public TAN: string;
+    constructor(private differs: KeyValueDiffers,
+        private cdr: ChangeDetectorRef,
+        private createPageService: CreatePageService) {
+        this.blockUI.start("Loading...");
+        this.isInit = false;
+    }
+
 
     ngOnInit(): void {
-    }
-    handleShowPdf () :void {
-
-    }
-    ngAfterViewInit(): void {
-        //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-        //Add 'implements AfterViewInit' to the class.
-        $(".selector-vaccine-id").selectpicker();
-        $(".selector-vaccine-id").selectpicker('val', this.selectedVaccItem.id);
-        $(".selector-vaccine-dose-number").selectpicker();
-        $(".selector-vaccine-dose-number").selectpicker('val', 1);
-        $(".selector-agency").selectpicker();
-        $(".selector-agency").selectpicker('val' , `${this.agencyItem[0].name}-${this.agencyItem[0].code}`);
-        this.birthDateicker = $('#inputBirthDate').datepicker({
-            format: "yyyy-mm-dd", //設定格式為2019-04-01
-            autoclose: true,//選擇日期後就會自動關閉
-            language: 'zh-TW'//中文化
-        }).on('changeDate', (e) => {
-            this.data.Birthday = e.format();
-        }).on('hide', (e) => {
-            this.data.Birthday = e.format();
-        });
-        this.dateOfVaccinationDatePicker = $('#inputDateOfVaccination').datepicker({
-            format: "yyyy-mm-dd", //設定格式為2019-04-01
-            autoclose: true,//選擇日期後就會自動關閉
-            language: 'zh-TW',//中文化m
-            endDate: new Date()
-        }).on('changeDate', (e) => {
-            this.data.InocuDate = e.format();
-            //$("#inputDateOfVaccination").val(e.format());
-        }).on('hide', (e) => {
-            this.data.InocuDate = e.format();
-        });
-        this.dateOfVaccinationDatePicker.datepicker('setDate', 'now');
-        let savedData = localStorage.getItem("dgcCreateData");
-        if (savedData) {
-            this.data = JSON.parse(savedData);
-            let dataValidation = this.dataJoi.validate(this.data);
-            if (dataValidation.error) { 
-                this.clearData();
-            } else {
-                this.selectedVaccItem = vaccineMap.find(v=> v.id == this.data.VaccID);
-                $(".selector-vaccine-id").selectpicker("val" , this.selectedVaccItem.id);
-                $(".selector-vaccine-dose-number").selectpicker("val" , this.data.VaccDoses);
-                $(".selector-agency").selectpicker("val" , this.data.AgencyCode);
-                const vacc: VaccinationEntry = {
-                    tg: "840539006",
-                    vp: this.selectedVaccItem.type.id,
-                    mp: this.selectedVaccItem.id,
-                    ma: this.selectedVaccItem.org.id,
-                    dn: Number(this.data.VaccDoses),
-                    sd: 2,
-                    dt: this.data.InocuDate,
-                    co: "TW",
-                    is: this.data.AgencyCode,
-                    ci: ''
-                };
-                this.eudgc = Object.assign({} ,{
-                    ver: '1.3.0',
-                    nam: {
-                        fn: undefined,
-                        fnt: this.data.Name!,
-                        gn: undefined,
-                        gnt: undefined
-                    },
-                    dob: this.data.Birthday,
-                    v: [vacc]
-                });
-                this.onVaccItemChange();
-                this.cdr.detectChanges();
-            }
-        }
-        this.cdr.detectChanges();
-    }
-
-    clearData() : void {
+        this.qrCode = "1";
+        this.cdcData = {
+            AgencyCode: undefined,
+            Data: undefined
+        };
+        this.dataSchema = {
+            AgencyCode: joi.required(),
+            IdNo: joi.string().required(),
+            Name: joi.string().required(),
+            Birthday: joi.string().required(),
+            InocuDate: joi.string().required(),
+            VaccID: joi.string().required(),
+            VaccDoses: joi.number().min(1).required()
+        };
+        this.dataJoi = joi.object(this.dataSchema);
         this.data = {
             AgencyCode: undefined,
             IdNo: undefined,
@@ -156,14 +68,103 @@ export class CreatePageComponent implements OnInit {
             VaccID: undefined,
             VaccDoses: 1
         }
-        this.ngAfterViewInit();
+        this.selectedVaccItem = {
+            "name": "",
+            "id": "",
+            "type": {
+                "id": "",
+                "name": ""
+            },
+            "org": {
+                "id": "",
+                "name": ""
+            }
+        };
+    }
+    init(): void {
+        //#region init selectpicker
+        $(".selector-vaccine-id").selectpicker();
+        $(".selector-vaccine-id").selectpicker('val', this.selectedVaccItem.id);
+        $(".selector-vaccine-dose-number").selectpicker();
+        $(".selector-vaccine-dose-number").selectpicker('val', 1);
+        $(".selector-agency").selectpicker();
+        $(".selector-agency").selectpicker('val', `${this.agencyItem[0].name}-${this.agencyItem[0].code}`);
+        this.data.AgencyCode = `${this.agencyItem[0].name}-${this.agencyItem[0].code}`;
+        //#endregion
+
+        //#region 初始化生日日期picker
+        this.birthDateicker = $('#inputBirthDate').datepicker({
+            format: "yyyy-mm-dd", //設定格式為2019-04-01
+            autoclose: true,//選擇日期後就會自動關閉
+            language: 'zh-TW'//中文化
+        }).on('changeDate', (e) => {
+            this.data.Birthday = e.format();
+        }).on('hide', (e) => {
+            this.data.Birthday = e.format();
+        });
+        //#endregion
+
+        //#region 初始化接種日期picker
+        this.dateOfVaccinationDatePicker = $('#inputDateOfVaccination').datepicker({
+            format: "yyyy-mm-dd", //設定格式為2019-04-01
+            autoclose: true,//選擇日期後就會自動關閉
+            language: 'zh-TW',//中文化
+            endDate: new Date() //最高只能選到當天
+        }).on('changeDate', (e) => {
+            this.data.InocuDate = e.format();
+            //$("#inputDateOfVaccination").val(e.format());
+        }).on('hide', (e) => {
+            this.data.InocuDate = e.format();
+        });
+        //預設今天
+        this.dateOfVaccinationDatePicker.datepicker('setDate', 'now');
+        //#endregion
+
+        this.cdr.detectChanges();
+    }
+    ngAfterViewInit(): void {
+        this.init();
+        //取得localStorage儲存的上一次接種資料
+        let savedData = localStorage.getItem("dgcCreateData");
+        if (savedData) {
+            this.data = JSON.parse(savedData);
+
+            let dataValidation = this.dataJoi.validate(this.data);
+            if (dataValidation.error) {
+                this.clearData();
+            } else {
+                this.selectedVaccItem = vaccineMap.find(v => v.id == this.data.VaccID);
+                $(".selector-vaccine-id").selectpicker("val", this.selectedVaccItem.id);
+                $(".selector-vaccine-dose-number").selectpicker("val", this.data.VaccDoses);
+                $(".selector-agency").selectpicker("val", this.data.AgencyCode);
+                this.onVaccItemChange();
+            }
+        }
+
+        setTimeout(() => {
+            this.blockUI.stop();
+            this.isInit = true;
+            this.cdr.detectChanges();
+        }, 2500);
+    }
+
+    clearData(): void {
+        this.data = {
+            AgencyCode: undefined,
+            IdNo: undefined,
+            Name: undefined,
+            Birthday: undefined,
+            InocuDate: undefined,
+            VaccID: undefined,
+            VaccDoses: 1
+        }
+        this.init();
         localStorage.removeItem('dgcCreateData');
     }
+
     onCreationClick(): void {
         this.blockUI.start("建立中...");
-        localStorage.setItem("dgcCreateData" , JSON.stringify(this.data));
         let dataValidation = this.dataJoi.validate(this.data);
-        console.log(this.data);
         if (dataValidation.error) {
             this.blockUI.resetGlobal();
             let detail = dataValidation.error.details.pop();
@@ -178,70 +179,75 @@ export class CreatePageComponent implements OnInit {
             }
             return;
         }
-        if (!verifyTWIdentifier(this.data.IdNo)) {
-            this.blockUI.resetGlobal();
-            Swal.fire('Oops...', "身份證字號格式錯誤", 'error');
-            return;
-        }
         this.cdcData.AgencyCode = this.data.AgencyCode;
         this.cdcData.Data = [];
         this.cdcData.Data.push(this.data);
-        this.createPageService.postItem(this.data).subscribe(
-            res=> {
-                this.doGenEDGCQE()
-                //location.reload();
-            },
-            err => {
-                this.blockUI.resetGlobal();
-                Swal.fire('Oops...', "伺服器發生錯誤", 'error');
-                console.error(err);
-            }
-        );
+        this.doGenEDGCQE().then(() => {
+            console.log("do Gen EDGCQE");
+            this.createPageService.postItem(this.data).subscribe(
+                res => {
+                    //防止按旁邊或按esc關閉modal
+                    $("#modalQrCode").modal({
+                        backdrop: 'static',
+                        keyboard: false
+                    });
+                    console.log(this.eudgc);
+                    localStorage.setItem("dgcCreateData", JSON.stringify(this.data));
+                },
+                err => {
+                    this.blockUI.resetGlobal();
+                    Swal.fire('Oops...', "伺服器發生錯誤", 'error');
+                    console.error(err);
+                }
+            );
+        })
+
     }
 
-    private doGenEDGCQE () {
-        const vacc: VaccinationEntry = {
-            tg: "840539006",
-            vp: this.selectedVaccItem.type.id,
-            mp: this.selectedVaccItem.id,
-            ma: this.selectedVaccItem.org.id,
-            dn: Number(this.data.VaccDoses),
-            sd: 2,
-            dt: this.data.InocuDate,
-            co: "TW",
-            is: this.data.AgencyCode,
-            ci: ''
-        };
-        this.eudgc = {
-            ver: '1.3.0',
-            nam: {
-                fn: undefined,
-                fnt: this.data.Name!,
-                gn: undefined,
-                gnt: undefined
-            },
-            dob: this.data.Birthday,
-            v: [vacc]
-        }
-        genEDGCQR(this.eudgc)
-        .then((certResult: CertResult) => {
-            //console.log("qrcode: " + certResult.qrCode);
-            this.qrCode = certResult.qrCode;
-            this.TAN = certResult.tan;
-            this.cdr.detectChanges();
-            $("#modalQrCode").modal('show');
-            console.log(vacc)
-            console.log(this.eudgc);
-            this.blockUI.resetGlobal();
-        })
-        .catch(error => {
-            console.error(error);
-            this.blockUI.resetGlobal();
+    private doGenEDGCQE(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const vacc: VaccinationEntry = {
+                tg: "840539006",
+                vp: this.selectedVaccItem.type.id,
+                mp: this.selectedVaccItem.id,
+                ma: this.selectedVaccItem.org.id,
+                dn: Number(this.data.VaccDoses),
+                sd: 2,
+                dt: this.data.InocuDate,
+                co: "TW",
+                is: this.data.AgencyCode,
+                ci: ''
+            };
+            this.eudgc = {
+                ver: '1.3.0',
+                nam: {
+                    fn: undefined,
+                    fnt: this.data.Name!,
+                    gn: undefined,
+                    gnt: undefined
+                },
+                dob: this.data.Birthday,
+                v: [vacc]
+            }
+            genEDGCQR(this.eudgc)
+                .then((certResult: CertResult) => {
+                    //console.log("qrcode: " + certResult.qrCode);
+                    this.qrCode = certResult.qrCode;
+                    this.TAN = certResult.tan;
+                    this.cdr.detectChanges();
+                    this.blockUI.resetGlobal();
+                    return resolve(true);
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.blockUI.resetGlobal();
+                    return reject(false);
+                });
         });
     }
 
     joiTest(field: string) {
-        let validation : joi.ValidationResult = this.dataSchema[field].validate(this.data[field]);
+        let validation: joi.ValidationResult = this.dataSchema[field].validate(this.data[field]);
         if (validation.error) {
             return false;
         }
@@ -260,13 +266,11 @@ export class CreatePageComponent implements OnInit {
     ngDoCheck(): void {
         //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
         //Add 'implements DoCheck' to the class.
-        $("qrcode canvas").each(function(index){
-            $(this).attr("id" ,"qr-code-pdf");
+        //給react用
+        $("qrcode canvas").each(function (index) {
+            $(this).attr("id", "qr-code-pdf");
         });
     }
 
-    onTestBtnClick() : void {
-        qrcodeDataDecoder(this.qrCode);
-    }
 }
 
